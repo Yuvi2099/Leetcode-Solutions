c74d6b5e5687d48a2b3e4264986d7b6827cb9af5
Yuvraj <yuvraj.singh0317@gmail.com>
2026-01-27 14:22:16 +0530

    Time: 0 ms (100%), Space: 42.3 MB (14.74%) - LeetHub

---
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.java
@@ -0,0 +1,6 @@
+class Solution {
+    public boolean isStrictlyPalindromic(int n) {
+        //only num=1 is strictly palindrome for all bases , all other nums will generate false;
+        return false;
+    }
+}
