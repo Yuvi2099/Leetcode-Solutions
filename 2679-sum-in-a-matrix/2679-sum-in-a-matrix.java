f091e6a139bf3bad853ee43654ba56618547d068
Yuvraj <yuvraj.singh0317@gmail.com>
2026-02-21 23:30:01 +0530

    Time: 17 ms (98.15%), Space: 77.6 MB (98.56%) - LeetHub

--- 2679-sum-in-a-matrix/2679-sum-in-a-matrix.java
+++ 2679-sum-in-a-matrix/2679-sum-in-a-matrix.java
@@ -3,14 +3,14 @@ class Solution {
         for(int[]arr:nums){
             Arrays.sort(arr);
         }
-        int n=nums[0].length-1;
         int sum=0;
+        int n=nums[0].length-1;
         for(int i=n;i>=0;i--){
             int max=Integer.MIN_VALUE;
             for(int j=0;j<nums.length;j++){
                 max=Math.max(max,nums[j][i]);
             }
-            sum=sum+max;
+            sum+=max;
         }
         return sum;
     }
