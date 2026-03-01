fd1382c3ac6a1a46d7be535dc89bccb39f52c49c
Yuvraj <yuvraj.singh0317@gmail.com>
2026-03-02 00:59:10 +0530

    Time: 4 ms (96.1%), Space: 47.3 MB (95.35%) - LeetHub

---
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minPartitions(String n) {
+        int max=0;
+        int len=n.length();
+        if(len==1){
+            return n.charAt(0)-'0';
+        }
+        for(int i=1;i<len;i++){
+            char ch=n.charAt(i);
+            if(max<ch-'0'){
+                max=ch-'0';
+            }
+        }
+        if(max<n.charAt(0)-'0'){
+            return n.charAt(0)-'0';
+        }
+        return max;
+    }
+}
