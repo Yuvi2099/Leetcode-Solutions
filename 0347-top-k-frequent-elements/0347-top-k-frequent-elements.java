c4f37be08adb25392c95556755a6aa110b907d81
Yuvraj <yuvraj.singh0317@gmail.com>
2026-03-04 00:04:52 +0530

    Time: 20 ms (8.4%), Space: 47.8 MB (48.89%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,28 +1,31 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         HashMap<Integer,Integer>m=new HashMap<>();
-        for(int i:nums){
-            m.put(i,m.getOrDefault(i,0)+1);
+        for(int i=0;i<nums.length;i++){
+            m.put(nums[i],m.getOrDefault(nums[i],1)+1);
         }
-        int[]arr=new int[k];
-        List<Integer>l=new ArrayList<>();
+        List<Integer>v=new ArrayList<>();
+        List<Integer>key=new ArrayList<>();
         for(int i:m.keySet()){
-            l.add(i);
+            v.add(m.get(i));
+            key.add(i);
         }
+        int[]res=new int[k];
         int count=0;
-        while(count<k){
+        while(count!=k){
             int max=0;
             int idx=0;
-            for(int i=0;i<l.size();i++){
-                if(m.get(l.get(i))>max){
-                    max=m.get(l.get(i));
+            for(int i=0;i<v.size();i++){
+                if(max<v.get(i)){
+                    max=v.get(i);
                     idx=i;
                 }
             }
-            arr[count]=l.get(idx);
+            res[count]=key.get(idx);
             count++;
-            l.remove(l.get(idx));
+            v.remove(idx);
+            key.remove(idx);
         }
-        return arr;
+        return res;
     }
 }
