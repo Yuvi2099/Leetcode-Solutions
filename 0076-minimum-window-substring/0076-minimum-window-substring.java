c32b5c17a5d7e972f6bfde514f96ac73c0b45798
Yuvraj <yuvraj.singh0317@gmail.com>
2026-03-13 00:22:10 +0530

    Time: 20 ms (39.09%), Space: 47.8 MB (30.64%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,36 +1,35 @@
 class Solution {
     public String minWindow(String s, String t) {
         if(t.length()>s.length()) return "";
-        else if(t.length()==s.length() && t.equals(s)) return s;
-        int l=s.length()-1;
-        int r=s.length()-1;
-        while(l!=0){
-            String x=s.substring(l,r+1);
-            boolean ok=true;
-            for(int i=0;i<t.length();i++){
-                char ch=t.charAt(i);
-                if(x.indexOf(ch)==-1){
-                    ok=false;
-                    break;
-                }
-            }
-            if(ok==true) return x;
-            l--;
+        HashMap<Character,Integer>m=new HashMap<>();
+        for(char ch:t.toCharArray()){
+            m.put(ch,m.getOrDefault(ch,0)+1);
         }
-        r=0;
-        while(r!=s.length()){
-            String x=s.substring(l,r+1);
-            boolean ok=true;
-            for(int i=0;i<t.length();i++){
-                char ch=t.charAt(i);
-                if(x.indexOf(ch)==-1){
-                    ok=false;
-                    break;
-                }
+        HashMap<Character,Integer>w=new HashMap<>();
+        int need=m.size();
+        int min=Integer.MAX_VALUE;
+        int have=0;
+        int l=0,i=0;
+        for(int r=0;r<s.length();r++){
+            char ch=s.charAt(r);
+            w.put(ch,w.getOrDefault(ch,0)+1);
+            if(m.containsKey(ch) && w.get(ch).intValue()==m.get(ch).intValue()){
+                have++;
             }
-            if(ok==true) return x;
-            r++;
+            while(have==need){
+                if(r-l+1<min){
+                    min=r-l+1;
+                    i=l;
+                }
+                char lc=s.charAt(l);
+                w.put(lc,w.get(lc)-1);
+                if(m.containsKey(lc) && w.get(lc)<m.get(lc)){
+                    have--;
+                }
+                l++;
+            }
         }
-        return "";
+        if(min==Integer.MAX_VALUE) return "";
+        return s.substring(i,i+min);
     }
 }
