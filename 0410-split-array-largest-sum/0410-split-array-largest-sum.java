a1ea41a95d76446e7117bf15658dc9c6c181bd19
Yuvraj <yuvraj.singh0317@gmail.com>
2026-03-19 11:48:07 +0530

    Time: 0 ms (100%), Space: 42.9 MB (80.53%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.java
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.java
@@ -1,40 +1,34 @@
 class Solution {
-    public int splitArray(int[] nums, int k) {
-        int n=nums.length;
-        int max=nums[0];
-        int[]prefix=new int[n];
-        prefix[0]=nums[0];
-        for(int i=1;i<n;i++){
-            prefix[i]=prefix[i-1]+nums[i];
-            max=Math.max(max,nums[i]);
-        }
-        if(k==1) return prefix[n-1];
-        else if(k==n) return max;
-        int l=max,r=prefix[n-1];
-        int ans=r;
-        while(l<=r){
-            int mid=l+(r-l)/2;
-            if(split(nums,k,mid)){
-                ans=mid;
-                r=mid-1;
-            }
-            else{
-                l=mid+1;
-            }
-        }
-        return ans;
-    }
-    private boolean split(int[]nums, int k, int limit){
+    public boolean split(int[] nums, int limit, int k){
         int count=1;
         int sum=0;
         for(int i:nums){
-            if(sum+i>limit){
+            if((sum+i)>limit){
                 count++;
                 sum=i;
-                if(count>k) return false;
             }
             else sum+=i;
         }
-        return true;
+        return count<=k;
+    }
+    public int splitArray(int[] nums, int k) {
+        int n=nums.length;
+        int max=0;
+        int min=Integer.MIN_VALUE;
+        if(n<k) return 0;
+        for(int i:nums){
+            min=Math.max(min,i);
+            max+=i;
+        }
+        int ans=0;
+        while(min<=max){
+            int mid=min+(max-min)/2;
+            if(split(nums,mid,k)){
+                ans=mid;
+                max=mid-1;
+            }
+            else min=mid+1;
+        }
+        return ans;
     }
 }
