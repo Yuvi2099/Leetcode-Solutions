9d55e10f3218fcfdae146d7349a9a1f127e219d3
Yuvraj <yuvraj.singh0317@gmail.com>
2026-04-01 23:54:07 +0530

    Time: 957 ms (5.23%), Space: 41.8 MB (94.77%) - LeetHub

---
+++ 0991-broken-calculator/0991-broken-calculator.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int brokenCalc(int startValue, int target) {
+        int count=0;
+        while(target!=startValue){
+            if(target>startValue && target%2==0){
+                target/=2;
+            }
+            else target+=1; 
+            count++;
+        }
+        return count;
+    }
+}
