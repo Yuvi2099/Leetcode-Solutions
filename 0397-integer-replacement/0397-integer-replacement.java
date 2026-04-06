647f24b95e9ea2c54958739fc598f6346f629870
Yuvraj <yuvraj.singh0317@gmail.com>
2026-04-06 16:36:27 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0397-integer-replacement/0397-integer-replacement.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int integerReplacement(int n) {
+        int ans=0;
+        while(n!=1){
+            if(n%2==0) n/=2;
+            else if(n==3 || (n-1)%4==0) n--;
+            else n++;
+            ans++;
+        }
+        return ans;
+    }
+}
