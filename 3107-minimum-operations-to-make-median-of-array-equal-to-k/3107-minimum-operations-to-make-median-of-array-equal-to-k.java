74fc678970f0965767172da2265e453ecee9ab66
Yuvraj <yuvraj.singh0317@gmail.com>
2026-04-08 23:32:39 +0530

    Time: 32 ms (92.16%), Space: 99.3 MB (38.24%) - LeetHub

---
+++ 3107-minimum-operations-to-make-median-of-array-equal-to-k/3107-minimum-operations-to-make-median-of-array-equal-to-k.java
@@ -0,0 +1,19 @@
+class Solution {
+    public long minOperationsToMakeMedianK(int[] nums, int k) {
+        Arrays.sort(nums);
+        int n=nums.length;
+        int mid=n/2;
+        long ans=0;
+        if(nums[mid]<k){
+            for(int i=mid;i<n;i++){
+                if(nums[i]<k) ans+=(long)(k-nums[i]);
+            }
+        }
+        else if(nums[mid]>k){
+            for(int i=0;i<=mid;i++){
+                if(nums[i]>k) ans+=(long)(nums[i]-k);
+            }
+        }
+        return ans;
+    }
+}
