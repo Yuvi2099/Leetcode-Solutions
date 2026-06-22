9213852ec4a1a44bf7496438dda4d0253d4a3341
Yuvraj <yuvraj.singh0317@gmail.com>
2026-06-22 13:14:46 +0530

    Time: 1 ms (27.2%), Space: 45.3 MB (38.5%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.java
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.java
@@ -9,8 +9,8 @@ class Solution {
         return prod;
     }
     private int signFunc(int x){
-        if(x==0) return 0;
+        if(x<0) return -1;
         else if(x>0) return 1;
-        return -1;
+        return 0;
     }
 }
