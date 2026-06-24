0f5afbdf28001a8c8116f384dbf7072e8df17ada
Yuvraj <yuvraj.singh0317@gmail.com>
2026-06-24 22:58:09 +0530

    Time: 92 ms (84.46%), Space: 51.3 MB (36.22%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -1,12 +1,13 @@
 class Solution {
     public int countPrimes(int n) {
-        boolean[] seen = new boolean[n];
-        int ans = 0;
-        for (int num = 2; num < n; num++) {
-            if (seen[num]) continue;
-            ans += 1;
-            for (long mult = (long)num * num; mult < n; mult += num)
-                seen[(int)mult] = true;
+        boolean[]seen=new boolean[n];
+        int ans=0;
+        for(int i=2;i<n;i++){
+            if(seen[i]==true) continue;
+            ans++;
+            for(long j=(long)i*i;j<n;j=j+i){
+                seen[(int)j]=true;
+            }
         }
         return ans;
     }
