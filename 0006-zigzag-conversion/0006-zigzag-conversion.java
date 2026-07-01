07382a05e995eedea0b43fa19484b991761df162
Yuvraj <yuvraj.singh0317@gmail.com>
2026-07-01 10:09:40 +0530

    Time: 16 ms (8.07%), Space: 47.4 MB (12.45%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String convert(String s, int numRows) {
+        if(numRows==1) return s;
+        HashMap<Integer,String>m=new HashMap<>();
+        for(int i=1;i<=numRows;i++) m.put(i,"");
+        int k=1;
+        boolean peak=false;
+        for(int i=0;i<s.length();i++){
+            String t=String.valueOf(s.charAt(i));
+            m.put(k,m.get(k)+t);
+            if(k==numRows) peak=true;
+            else if(k==1) peak=false;
+            if(peak) k--;
+            else k++;
+        } 
+        StringBuilder sb=new StringBuilder();
+        for(int x:m.keySet()){
+            sb.append(m.get(x));
+        }
+        return sb.toString();
+    }
+}
