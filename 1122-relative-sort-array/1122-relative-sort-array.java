866008110e0c0281f54784b5b2a59209f9450f96
Yuvraj <yuvraj.singh0317@gmail.com>
2026-07-06 13:20:14 +0530

    Time: 0 ms (100%), Space: 43.4 MB (95.94%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.java
+++ 1122-relative-sort-array/1122-relative-sort-array.java
@@ -1,22 +1,23 @@
 class Solution {
     public int[] relativeSortArray(int[] arr1, int[] arr2) {
-        HashMap<Integer,Integer>m=new HashMap<>();
-        for(int i:arr1) m.put(i,m.getOrDefault(i,0)+1);
-        List<Integer>ans=new ArrayList<>();
-        for(int i=0;i<arr2.length;i++){
-            int count=m.get(arr2[i]);
-            for(int j=0;j<count;j++) ans.add(arr2[i]);
+        int[]freq=new int[1001];
+        for(int i:arr1) freq[i]++;
+        int[]result=new int[arr1.length];
+        int idx=0;
+        for(int i:arr2){
+            while(freq[i]>0){
+                result[idx]=i;
+                freq[i]--;
+                idx++;
+            }
         }
-        List<Integer>extra=new ArrayList<>();
-        for(int i:arr1){
-            if(!ans.contains(i)) extra.add(i);
+        for(int i=0;i<freq.length;i++){
+            while(freq[i]>0){
+                result[idx]=i;
+                freq[i]--;
+                idx++;
+            }
         }
-        Collections.sort(extra);
-        for(int i:extra) ans.add(i);
-        int[]res=new int[ans.size()];
-        for(int i=0;i<res.length;i++){
-            res[i]=ans.get(i);
-        }
-        return res;
+        return result;
     }
 }
