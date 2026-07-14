8c6f4d5ff959bd656d05c6c0b29526f1d5aef2c9
Yuvraj <yuvraj.singh0317@gmail.com>
2026-07-14 09:14:58 +0530

    Time: 2 ms (89.83%), Space: 48 MB (6.76%) - LeetHub

--- 3211-generate-binary-strings-without-adjacent-zeros/3211-generate-binary-strings-without-adjacent-zeros.java
+++ 3211-generate-binary-strings-without-adjacent-zeros/3211-generate-binary-strings-without-adjacent-zeros.java
@@ -1,28 +1,21 @@
 class Solution {
     public List<String> validStrings(int n) {
         List<String>l=new ArrayList<>();
-        int d=(int)Math.pow(2,n);
-        for(int i=0;i<d;i++){
-            String x=Integer.toBinaryString(i);
-            if(x.length()!=n) x=construct(x,n);
-            if(valid(x)) l.add(x);
-        }
+        construct(new StringBuilder(),n,l);
         return l;
     }
-    public boolean valid(String x){
-        if(x.length()<2) return true;
-        for(int i=1;i<x.length();i++){
-            if(x.charAt(i)!='1' && x.charAt(i-1)!='1') return false;
+    private void construct(StringBuilder sb, int n, List<String> l){
+        if(sb.length()==n){
+            l.add(sb.toString());
+            return;
         }
-        return true;
-    }
-    public String construct(String x,int n){
-        int len=n-x.length();
-        StringBuilder sb=new StringBuilder();
-        for(int i=0;i<len;i++){
-            sb.append("0");
+        sb.append('1');
+        construct(sb,n,l);
+        sb.deleteCharAt(sb.length()-1);
+        if(sb.length()==0 || sb.charAt(sb.length()-1)=='1'){
+            sb.append('0');
+            construct(sb,n,l);
+            sb.deleteCharAt(sb.length()-1);
         }
-        sb.append(x);
-        return sb.toString();
     }
 }
