7bff8fffd0081f50e18d99c443d82ceb2b5d2472
Yuvraj <yuvraj.singh0317@gmail.com>
2026-07-15 09:58:22 +0530

    Time: 0 ms (100%), Space: 42.1 MB (59.14%) - LeetHub

--- 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.java
+++ 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.java
@@ -1,39 +1,18 @@
 class Solution {
     public int minFlips(int a, int b, int c) {
-        String x=Integer.toBinaryString(a);
-        String y=Integer.toBinaryString(b);
-        String z=Integer.toBinaryString(c);
-
-        int max=0,len=z.length();
-        max=Math.max(a,Math.max(b,c));
-
-        if(max==a) len=x.length();
-        else if(max==b) len=y.length();
-
-        x=construct(x,len-x.length());
-        y=construct(y,len-y.length());
-        z=construct(z,len-z.length());
-
         int count=0;
-        for(int i=len-1;i>=0;i--){
-            char ah=x.charAt(i);
-            char bh=y.charAt(i);
-            char ch=z.charAt(i);
-            if(ch=='0'){
-                if(ah!='0') count++;
-                if(bh!='0') count++;
-            }
+        while(a>0 || b>0 || c>0){
+            int ab=a&1;
+            int bb=b&1;
+            int cb=c&1;
+            if(cb==0) count+=ab+bb;
             else{
-                if((ah=='1' && bh=='0') || (ah=='0' && bh=='1')) continue;
-                else if(ah==bh && ah=='0') count++;
+                if(ab==0 && bb==0) count++;
             }
+            a>>=1;
+            b>>=1;
+            c>>=1;
         }
         return count;
-    }
-    public String construct(String s, int n){
-        StringBuilder sb=new StringBuilder();
-        for(int i=0;i<n;i++) sb.append("0");
-        sb.append(s);
-        return sb.toString();
     }
 }
