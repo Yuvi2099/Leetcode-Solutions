50e1e1005ba3c8c327cbcc79e087429907325071
Yuvraj <yuvraj.singh0317@gmail.com>
2026-07-23 11:13:17 +0530

    Time: 5 ms (6.48%), Space: 44.4 MB (9.77%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.java
+++ 1544-make-the-string-great/1544-make-the-string-great.java
@@ -2,21 +2,7 @@ class Solution {
     public String makeGood(String s) {
         Stack<Character>st=new Stack<>();
         for(char ch:s.toCharArray()){
-            if(!st.isEmpty()){
-                char bh=st.peek();
-                if(ch==bh) st.push(ch);
-                else if(Character.isLowerCase(ch) && Character.isUpperCase(bh)){
-                    char sh=Character.toLowerCase(bh);
-                    if(ch==sh) st.pop();
-                    else st.push(ch);
-                }
-                else if(Character.isLowerCase(bh) && Character.isUpperCase(ch)){
-                    char sh=Character.toUpperCase(bh);
-                    if(ch==sh) st.pop();
-                    else st.push(ch);
-                }
-                else st.push(ch);
-            }
+            if(!st.isEmpty() && Math.abs(ch-st.peek())==32) st.pop();
             else st.push(ch);
         }
         String x="";
