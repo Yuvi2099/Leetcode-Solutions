68845ed3e1babb144388922da87ae71520e0cd8c
Yuvraj <yuvraj.singh0317@gmail.com>
2026-07-28 16:10:17 +0530

    Time: 2 ms (100%), Space: 108.6 MB (16.59%) - LeetHub

--- 3638-maximum-balanced-shipments/3638-maximum-balanced-shipments.java
+++ 3638-maximum-balanced-shipments/3638-maximum-balanced-shipments.java
@@ -2,8 +2,8 @@ class Solution {
     public int maxBalancedShipments(int[] weight) {
         int max=0,count=0;
         for(int i=0;i<weight.length;i++){
-            max=Math.max(max,weight[i]);
-            if(weight[i]<max){
+            if(max<=weight[i]) max=weight[i];
+            else{
                 count++;
                 max=0;
             }
