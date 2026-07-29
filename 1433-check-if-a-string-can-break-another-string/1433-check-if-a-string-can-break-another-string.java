728e469de1db1138230c8848b99d809340acc7e0
Yuvraj <yuvraj.singh0317@gmail.com>
2026-07-29 17:28:42 +0530

    Time: 8 ms (80%), Space: 48.4 MB (6.54%) - LeetHub

---
+++ 1433-check-if-a-string-can-break-another-string/1433-check-if-a-string-can-break-another-string.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean checkIfCanBreak(String s1, String s2) {
+        char[]arr=s1.toCharArray();
+        char[]brr=s2.toCharArray();
+        Arrays.sort(arr);
+        Arrays.sort(brr);
+        boolean ans=true;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]<=brr[i]) continue;
+            else{
+                ans=false;
+                break;
+            }
+        }
+        if(ans) return ans;
+        for(int i=0;i<arr.length;i++){
+            if(brr[i]<=arr[i]) continue;
+            else return false;
+        }
+        return true;
+    }
+}
