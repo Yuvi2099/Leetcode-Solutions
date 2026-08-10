12623010f57278cee40f4b2e03125418138e368a
Yuvraj <yuvraj.singh0317@gmail.com>
2026-08-10 21:12:25 +0530

    Time: 45 ms (24.54%), Space: 50.8 MB (41.25%) - LeetHub

---
+++ 1510-stone-game-iv/1510-stone-game-iv.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean winnerSquareGame(int n) {
+        int[]dp=new int[n+1];
+        Arrays.fill(dp,-1);
+        return solveForAlice(n,dp);
+    }
+    private boolean solveForAlice(int n, int[]dp){
+        if(n==0) return false;
+        if(dp[n]!=-1) return dp[n]==1;
+        for(int i=1;i*i<=n;i++){
+            if(!solveForAlice(n-i*i,dp)){
+                dp[n]=1;
+                return true;
+            }
+        }
+        dp[n]=0;
+        return false;
+    }
+}
