c4fcaf849d8d97706b9e4225c9f01a4fe33bc9f3
Yuvraj <yuvraj.singh0317@gmail.com>
2026-09-07 15:56:18 +0530

    Time: 0 ms (100%), Space: 44.7 MB (93.4%) - LeetHub

---
+++ 2960-count-tested-devices-after-test-operations/2960-count-tested-devices-after-test-operations.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int countTestedDevices(int[] b) {
+        int c=0;
+        for(int i=0;i<b.length;i++){
+            if(b[i]-c>0) c++;
+        }
+        return c;
+    }
+}
